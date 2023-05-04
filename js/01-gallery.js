d1b3fefd14c4349e9d4439c3fce6e8fcd67e4fd3
Bliznech <120974217+Bliznech@users.noreply.github.com>
2023-05-04 22:32:44 +0200

    +-+

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,8 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const instance = basicLightbox.create(`
+    <img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg" width="800" height="600">
+`);
 
+instance.show();
 console.log(galleryItems);
